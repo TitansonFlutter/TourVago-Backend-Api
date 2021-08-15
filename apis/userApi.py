832c9108f22ccf52f22bbe8a721c749db7aff700
Abenezer Fekadu <50666961+Abenezer-Fekadu@users.noreply.github.com>
2021-08-15 20:57:06 +0300

    Created user history Apis. (#9)

--- apis/userApi.py
+++ apis/userApi.py
@@ -1,6 +1,7 @@
 # Import modules
-from flask_restplus import Resource, Namespace
-from flask import Flask
+from flask_restplus import fields, Resource, Namespace
+from flask import request, Flask
+import datetime
 
 from Model.models import *
 from marsh import *
@@ -13,26 +14,90 @@ app = Flask(__name__)
 namespace3 = Namespace("users", description="Users related operations")
 
 
+# Book Data Model
+book = namespace3.model(
+    "Book",
+    {"TourId": fields.Integer},
+)
+
+
 # Users Api
 @namespace3.route("/userId:int/book")
 class UserBookResource(Resource):
+    @namespace3.expect(book)
     def post(self, userId):
         """
         Book A Tour
         """
+        # Get Tour ID from Api
+        tourId = request.json["TourId"]
+
+        # Search if the Tour Exists
+        tour = Tours.query.filter_by(TourId=tourId).first()
+
+        # if no tour
+        if not tour:
+            return {"message": "The Tour Doesn't Exist"}, 400
+
+        # create new Book
+        new_book = Book(
+            UserId=userId,
+            TourId=tourId,
+        )
+
+        # Add to database
+        db.session.add(new_book)
+        db.session.commit()
 
 
 @namespace3.route("/userId:int/history/upcoming")
-class UserUpcomingHistoryResource(Resource):
+class UserUpcomingHistoriesResource(Resource):
     def get(self, userId):
         """
         Get Upcoming History
         """
 
+        # Get all user book histories
+        histories = Book.query.filter_by(UserId=userId).all()
+
+        # If not booked yet
+        if not histories:
+            return {"message": "You have not Booked Yet"}, 400
+
+        # upcoming list
+        upcoming = []
+        # Check DateTime and if greater add to upcoming list
+        for history in histories:
+            date = history.StartingDate
+            if date >= datetime.datetime.now():
+                # Get Tour Details from Database
+                tour = Tours.query.filter_by(TourId=history.TourId)
+                upcoming.append(tour)
+
+        return upcoming
+
 
 @namespace3.route("/userId:int/history/past")
-class UserPastHistoryResource(Resource):
+class UserPastHistoriesResource(Resource):
     def get(self, userId):
         """
         Get Past History
         """
+        # Get all user book histories
+        histories = Book.query.filter_by(UserId=userId).all()
+
+        # If not booked yet
+        if not histories:
+            return {"message": "You have not Booked Yet"}, 400
+
+        # past list
+        past = []
+        # Check DateTime and if less, add to past list
+        for history in histories:
+            date = history.StartingDate
+            if date < datetime.datetime.now():
+                # Get Tour Details from Database
+                tour = Tours.query.filter_by(TourId=history.TourId)
+                past.append(tour)
+
+        return past
