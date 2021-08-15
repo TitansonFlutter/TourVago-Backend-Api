9a7b3277506f8e86cef86978fbc372e6eec20314
Tolosa-mitiku <61460697+Tolosa-mitiku@users.noreply.github.com>
2021-08-15 14:13:31 +0300

    Tolosa.model (#8)
    * The Book Model has been created
    
    * The History Model has been created
    
    * The Review Model has been created
    
    * The Tours Model has been created
    
    * The Models have been generally edited. History Model have been removed and forgotten attributes added while irrelevant ones discarded.
    
    * Final draft of the model has been implemented.

--- Model/models.py
+++ Model/models.py
@@ -17,8 +17,49 @@ class AgentInfo(db.Model):
     AgentInfoId = db.Column(db.Integer, primary_key=True)
     AgentId = db.Column(db.Integer, db.ForeignKey("Users.UserId"), nullable=False)
     Approved = db.Column(db.Boolean, nullable=False)
-    Acronyms = db.Column(db.String, nullable=False)
+    Acronym = db.Column(db.String, nullable=False)
     Motto = db.Column(db.String, nullable=False)
     Description = db.Column(db.String, nullable=False)
     Website = db.Column(db.String, nullable=False)
+    Country = db.Column(db.String, nullable=False)
     Region = db.Column(db.String, nullable=False)
+    City = db.Column(db.String, nullable=False)
+    Address = db.Column(db.String, nullable=False)
+    PhoneNumber = db.Column(db.String, nullable=False)
+    ZipCode = db.Column(db.String, nullable=False)
+
+class Book(db.Model):
+    __tablename__ = "book"
+    BookId = db.Column(db.Integer, primary_key=True)
+    UserId = db.Column(db.Integer, db.ForeignKey("Users.UserId"), nullable=False)
+    TourId = db.Column(db.Integer, db.ForeignKey("Tours.TourId"), nullable=False)
+
+class Review(db.Model):
+    __tablename__ = "review"
+    ReviewId = db.Column(db.Integer, primary_key=True)
+    Comment = db.Column(db.String, nullable=False)
+    Rate = db.Column(db.String, nullable=False)
+    Date = db.Column(db.String, nullable=False)
+    UserId = db.Column(db.Integer, db.ForeignKey("Users.UserId"), nullable=False)
+    BookId = db.Column(db.Integer, db.ForeignKey("Book.BookId"), nullable=False)
+
+class Tours(db.Model):
+    __tablename__ = "tours"
+    TourId = db.Column(db.Integer, primary_key=True)
+    TourName = db.Column(db.String, nullable=False)
+    TourImage = db.Column(db.String, nullable=False)
+    Country = db.Column(db.String, nullable=False)
+    Region = db.Column(db.String, nullable=False)
+    City = db.Column(db.String, nullable=False)
+    WhatIsIncluded = db.Column(db.String, nullable=False)
+    WhatIsExcluded = db.Column(db.String, nullable=False)
+    TourDescription = db.Column(db.String, nullable=False)
+    WhatToBring = db.Column(db.String, nullable=False)
+    Itinerary = db.Column(db.String, nullable=False)
+    Duration = db.Column(db.String, nullable=False)
+    StartingDate = db.Column(db.String, nullable=False)
+    Special = db.Column(db.String, nullable=False)
+    Price = db.Column(db.String, nullable=False)
+    Updated = db.Column(db.Boolean, nullable=False)
+    UserId = db.Column(db.Integer, db.ForeignKey("Users.UserId"), nullable=False)
+
