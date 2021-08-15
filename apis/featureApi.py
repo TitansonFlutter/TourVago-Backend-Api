4ef160d8e488fc5d513c0d8f410c9c30364eb8fb
mekides4325 <se.mekides.bishaw@gmail.com>
2021-08-15 19:46:16 +0300

    some implementation for feature API added

--- apis/featureApi.py
+++ apis/featureApi.py
@@ -1,6 +1,9 @@
 # Import modules
-from flask_restplus import Resource, Namespace
+from flask.globals import request
+from flask_restplus import Resource, Namespace, api
 from flask import Flask
+from flask_marshmallow import Marshmallow
+from marshmallow import *
 
 from Model.models import *
 from marsh import *
@@ -12,14 +15,58 @@ app = Flask(__name__)
 # Users Namespace
 namespace4 = Namespace("features", description="Features related operations")
 
+review_schema = ReviewSchema()
+reviews_schema = ReviewSchema(many=True)
+
+tour_schema = TourSchema()
+tours_schema = TourSchema(many=True)
+
+review = api.model("Review",{
+    "Comment": fields.String,
+    "Rate":fields.String,
+    "Date":fields.String,
+    "UserId":fields.Integer,
+    "BookId":fields.Integer,
+})
+
+tour = api.model("Review",{
+    "Comment": fields.String,
+    "Rate":fields.String,
+    "Date":fields.String,
+    "UserId":fields.Integer,
+    "BookId":fields.Integer,
+})
+
+
 
 # Features Api
 @namespace4.route("/review")
 class ReviewResource(Resource):
+    @namespace4.expect(review)
     def post(self):
         """
         Give Review to Tours
         """
+        new_review = Review()
+        new_review.Comment = request.json['Comment']
+        new_review.Rate = request.json['Rate']
+        new_review.UserId = request.json['UserId']
+        new_review.Date = request.json['Date']
+        new_review.BookId = request.json['BookId']
+
+        bookId = Book.query.filter_by(book_id=new_review.BookId).first()
+        userId = Book.query.filter_by(user_id=new_review.UserId).first()
+
+        if (bookId & userId):
+            db.session.add(new_review)
+            db.session.commit()
+            print(new_review)
+            return review_schema.dump(new_review),201
+
+        return 404
+
+        
+
 
 
 @namespace4.route("/topDestinations")
@@ -38,9 +85,15 @@ class RecommendedResource(Resource):
         """
 
 
+
 @namespace4.route("/tours/tourId:int")
 class ToursResource(Resource):
-    def get(self):
+    def get(self, tid):
         """
         Get Tour Details
         """
+        
+        tour = Tours.query.filter_by(book_id=tid).first()
+        if tour:
+            return tour_schema.dump(tour),200
+        return 404   
--- marsh.py
+++ marsh.py
@@ -10,3 +10,63 @@ class UsersSchema(mar.Schema):
         fields = ("UserId", "UserName", "Role", "Email")
 
         model = Users
+
+
+class Reviews:
+    UserId = 0
+    BookId = 0
+    Comment = ""
+    Rate = ""
+    Date = ""
+
+
+class ReviewSchema(mar.Schema):
+    class Meta:
+        fields = ("Comment", "Rate", "Date", "UserId", "BookId")
+        model = Reviews
+
+
+class Users:
+    UserId = 0
+    BookId = 0
+    Comment = ""
+    TourName = ""
+    TourImage = ""
+    Country = ""
+    Region = ""
+    City = ""
+    WhatIsIncluded = ""
+    WhatIsExcluded = ""
+    TourDescription = ""
+    WhatToBring = ""
+    Itinerary = ""
+    Duration = ""
+    StartingDate = ""
+    Special = ""
+    Price = ""
+    Updated = ""
+
+
+class TourSchema:
+    class Meta:
+        fields = (
+            "UserId",
+            "BookId",
+            "Comment",
+            "TourName",
+            "TourImage",
+            "Country",
+            "Region",
+            "City",
+            "WhatIsIncluded",
+            "WhatIsExcluded",
+            "TourDescription",
+            "WhatToBring",
+            "Itinerary",
+            "Duration",
+            "StartingDate",
+            "Special",
+            "Price",
+            "Updated"
+        )
+        model = Users
