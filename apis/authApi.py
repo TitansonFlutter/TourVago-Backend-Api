bdd125592cb16988080707aa59e3df45ec5a7aaa
Biruk Ayalew <se.biruk.ayalew@gmail.com>
2021-09-10 16:03:34 +0300

    Edit auth api

--- apis/authApi.py
+++ apis/authApi.py
@@ -3,14 +3,27 @@ from flask_restplus import Resource, fields, Namespace
 from flask import request, Flask
 from flask_bcrypt import Bcrypt
 
+# import jwt
+
+# from flask_jwt_extended import (
+#     create_access_token,
+#     get_jwt,
+#     jwt_required,
+#     get_jwt_identity,
+# )
+from datetime import datetime
 from Model.models import *
 from marsh import *
-
+import datetime
 
 # Instance app
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 
+
+app.config["SECRET_KEY"] = b'_5#y2L"F4Q8z\n\xec]/'
+# app.config["JWT_SECRET_KEY"] = b'_5#y2L"F4Q8z\n\xec]/'
+# app.config["JWT_BLACKLIST_ENABLED"] = ["access"]
 # Users Namespace
 namespace1 = Namespace("auth", description="Authentications related operations")
 
@@ -43,20 +56,37 @@ class UserResource(Resource):
         """
         Login user
         """
+        expires = datetime.timedelta(days=30)
+
         # Get data from api
         email = request.json["Email"]
         password = request.json["Password"]
-
+        print(email, password)
         # Get user data from database
         user = Users.query.filter_by(Email=email).first()
         # Check if the user exists
         if not user:
             return {"message": "The User is not Found"}, 404
         else:
+            role = user.Role
             # Decrypt password from  database and compare
             check = bcrypt.check_password_hash(user.Password, password)
             # If the password is correct return user
             if check:
+                additional_claims = {"role": role}
+                # token = jwt.encode(
+                #     {
+                #         "user": user.UserId,
+                #         "expiration": str(datetime.utcnow() + timedelta(seconds=60)),
+                #     },
+                #     app.config["SECRET_KEY"],
+                # )
+                # token = create_access_token(
+                #     identity=user.UserId,
+                #     expires_delta=expires,
+                #     additional_claims=additional_claims,
+                # )
+                # return {"token": token.decode("utf-8")}
                 return user_schema.dump(user)
             else:
                 return {"message": "The Password or Email is Incorrect"}, 400
@@ -95,4 +125,32 @@ class usersResource(Resource):
         db.session.add(new_user)
         db.session.commit()
         # Send user data
-        return user_schema.dump(new_user), 200
+        return user_schema.dump(new_user), 201
+
+
+@namespace1.route("/<int:id>")
+class usersResource(Resource):
+    def put(self, id):
+        """
+        Update User
+        """
+        email = request.json["Email"]
+        user = Users.query.filter_by(Email=email).first()
+        if user:
+            return {"message": "User already exists"}, 400
+
+        user = Users.query.filter_by(UserId=id).first()
+        if not user:
+            return {"message": "User not found"}, 404
+
+        password = request.json["Password"]
+        hashed = bcrypt.generate_password_hash(password).decode("utf-8")
+
+        # new_user = Users()
+        user.UserName = request.json["UserName"]
+        user.Role = request.json["Role"]
+        user.Email = request.json["Email"]
+        user.Password = hashed
+
+        db.session.commit()
+        return user_schema.dump(user)
