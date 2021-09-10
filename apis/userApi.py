9882b5e61a65692eebf7488a58aad9449d48ce0b
Mekides Bishaw <61588558+mekides4325@users.noreply.github.com>
2021-09-10 21:28:13 +0300

    Update userApi.py
    User API has been updated

--- apis/userApi.py
+++ apis/userApi.py
@@ -1,11 +1,13 @@
+
 # Import modules
 from flask_restplus import fields, Resource, Namespace
 from flask import request, Flask
-import datetime
+from datetime import datetime
 
 from Model.models import *
 from marsh import *
-
+from .agentApi import tour_schema, tours_schema
+from .authApi import user_schema
 
 # Instance app
 app = Flask(__name__)
@@ -23,7 +25,7 @@ book = namespace3.model(
 
 # Users Api
 @namespace3.route("/<int:userId>/book")
-class UserBookResource(Resource):
+class UsersBookResource(Resource):
     @namespace3.expect(book)
     def post(self, userId):
         """
@@ -34,11 +36,15 @@ class UserBookResource(Resource):
 
         # Search if the Tour Exists
         tour = Tours.query.filter_by(TourId=tourId).first()
+        print(tour)
 
         # if no tour
         if not tour:
             return {"message": "The Tour Doesn't Exist"}, 400
 
+        book = Book.query.filter_by(UserId=userId, TourId=tourId).first()
+        if book:
+            return {"message": "You have allready Booked"}, 400
         # create new Book
         new_book = Book(
             UserId=userId,
@@ -48,6 +54,21 @@ class UserBookResource(Resource):
         # Add to database
         db.session.add(new_book)
         db.session.commit()
+        return {"message": "Booked"}, 200
+
+
+@namespace3.route("/<int:userId>/book/<int:tourId>")
+class UserBookResource(Resource):
+    def get(self, userId, tourId):
+        """
+        book status
+        """
+        book = Book.query.filter_by(UserId=userId, TourId=tourId).first()
+        if book:
+            user = Users.query.filter_by(UserId=userId).first()
+
+            return user_schema.dump(user)
+        return {"message": "Not Booked"}, 404
 
 
 @namespace3.route("/<int:userId>/history/upcoming")
@@ -62,19 +83,24 @@ class UserUpcomingHistoriesResource(Resource):
 
         # If not booked yet
         if not histories:
-            return {"message": "You have not Booked Yet"}, 400
+            return {"message": "You have not Booked Yet"}, 404
 
         # upcoming list
         upcoming = []
-        # Check DateTime and if greater add to upcoming list
+        # # Check DateTime and if greater add to upcoming list
         for history in histories:
-            date = history.StartingDate
-            if date >= datetime.datetime.now():
+            tour = Tours.query.filter_by(TourId=history.TourId).first()
+
+            date = datetime.strptime(tour.StartingDate, "%Y-%m-%d %H:%M:%S.%f")
+            if date >= datetime.now():
                 # Get Tour Details from Database
-                tour = Tours.query.filter_by(TourId=history.TourId)
                 upcoming.append(tour)
 
-        return upcoming
+        # for history in histories:
+        #     tour = Tours.query.filter_by(TourId=history.TourId).all()
+        if not upcoming:
+            return {"message": "No Past Histories"}, 404
+        return tours_schema.dump(upcoming)
 
 
 @namespace3.route("/<int:userId>/history/past")
@@ -88,16 +114,62 @@ class UserPastHistoriesResource(Resource):
 
         # If not booked yet
         if not histories:
-            return {"message": "You have not Booked Yet"}, 400
+            return {"message": "You have not Booked Yet"}, 404
 
         # past list
         past = []
         # Check DateTime and if less, add to past list
         for history in histories:
-            date = history.StartingDate
-            if date < datetime.datetime.now():
-                # Get Tour Details from Database
-                tour = Tours.query.filter_by(TourId=history.TourId)
-                past.append(tour)
+            tour = Tours.query.filter_by(TourId=history.TourId).first()
 
-        return past
+            date = datetime.strptime(tour.StartingDate, "%Y-%m-%d %H:%M:%S.%f")
+            if date < datetime.now():
+                # Get Tour Details from Database
+                past.append(tour)
+        if not past:
+            return {"message": "No Past Histories"}, 404
+
+        return tours_schema.dump(past)
+
+
+
+
+
+
+
+   
+
+
+
+     
+  
+       
+        
+
+       
+        
+
+        
+        
+           
+       
+        
+            
+
+     
+        
+
+
+
+    
+
+       
+
+        
+        
+
+
+    
+            
+            
+           
